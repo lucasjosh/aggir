a00dc47fef7df0ad8709ffbdc6b09a697905d1b0
lucasjosh <jl@lucasjosh.com>
2009-03-08 13:47:02 -0700

    we can search...  nothing to search yet but it's a start

--- lib/aggir/solr.rb
+++ lib/aggir/solr.rb
@@ -1,14 +1,22 @@
+require 'httparty'
+
 module Aggir
   class Solr
+    include HTTParty
+    base_uri 'http://localhost:8080/aggir'
     
-   class << self
-     def update(solr_params)
-      
-     end
-     
-     def search(query_params)
-      
-     end
-   end 
+    
+   def update(options = {})
+    
+   end
+   
+   def search(options = {})
+     self.class.get('/select', options)      
+   end
+   
+   private
+   def add_field()
+    
+   end
   end
 end
