19b811f5922fd220087b6203cac393f14ba74b48
lucasjosh <jl@lucasjosh.com>
2009-03-08 14:57:06 -0700

    Start of Solr updating...

--- lib/aggir/solr.rb
+++ lib/aggir/solr.rb
@@ -6,17 +6,23 @@ module Aggir
     base_uri 'http://localhost:8080/aggir'
     
     
-   def update(options = {})
-    
+   def update(docs)
+    str = "<add>\n"
+    docs.each do |doc|
+      str << "<doc>\n"
+      doc.each {|k,v| str << add_field(k, v)}
+      str << "</doc>\n"
+    end
+    str << "</add>\n"
    end
    
-   def search(options = {})
-     self.class.get('/select', options)      
+   def search(query_params)
+     self.class.get('/select', {:query => {:q => query_params}})      
    end
    
    private
-   def add_field()
-    
+   def add_field(key, value)
+    "<field name=\"#{key}\">#{value}</field>\n"
    end
   end
 end
